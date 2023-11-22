10af7cf59853d8fff57d1aad67a3f3aa5b2d18fa
Egor <nounstep@gmail.com>
2023-11-22 15:16:00 +0300

    improved gui

--- chat_server.py
+++ chat_server.py
@@ -5,7 +5,6 @@ class ChatServer:
     def __init__(self):
         self.clients = set()
         self.rooms = {}  # Комнаты и их участники
-        self.message_history = {}  # История сообщений для каждой комнаты
 
     async def handle_client(self, websocket, path):
         self.clients.add(websocket)
@@ -14,10 +13,11 @@ class ChatServer:
             while True:
                 message = await websocket.recv()
                 if message.startswith('/auth'):
-                    await self.authenticate(websocket, message)
+                    username, room_name = message.split(' ')[1:3]
+                    self.join_room(websocket, username, room_name)
                 elif message.startswith('/join'):
                     room_name = message.split(' ')[1]
-                    await self.join_room(websocket, room_name)
+                    self.join_room(websocket, None, room_name)
                 else:
                     await self.broadcast(websocket, message)
         except websockets.exceptions.ConnectionClosedError:
@@ -28,12 +28,6 @@ class ChatServer:
             for room, members in self.rooms.items():
                 members.discard(websocket)
 
-    async def authenticate(self, client, message):
-        # Аутентификация пользователя
-        username = message.split(' ')[1]
-        print(f"User {username} authenticated.")
-        await client.send(f"Welcome, {username}!")
-
     async def broadcast(self, sender, message):
         # Отправка сообщения всем участникам комнаты
         for room, members in self.rooms.items():
@@ -41,12 +35,10 @@ class ChatServer:
                 for member in members:
                     try:
                         await member.send(message)
-                        # Сохранение сообщения в истории комнаты
-                        self.message_history.setdefault(room, []).append(message)
                     except:
                         continue
 
-    async def join_room(self, client, room_name):
+    def join_room(self, client, username, room_name):
         # Создание комнаты, если ее нет
         if room_name not in self.rooms:
             self.rooms[room_name] = set()
@@ -55,12 +47,13 @@ class ChatServer:
         for room, members in self.rooms.items():
             if client in members:
                 members.discard(client)
-        self.rooms[room_name].add(client)
 
-        # Отправка истории сообщений комнаты новому участнику
-        history = self.message_history.get(room_name, [])
-        for message in history:
-            await client.send(message)
+        if username:
+            self.rooms[room_name].add(client)
+            # Отправляем уведомление о входе в комнату только текущему пользователю
+            client.send(f"---Joined room: {room_name} as {username}---")
+        else:
+            self.rooms[room_name].add(client)
 
 async def main():
     chat_server = ChatServer()
